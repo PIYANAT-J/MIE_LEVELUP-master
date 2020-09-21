872fcb3f26b150b7d4feca861b6db6a15096b6b0
PIYANAT-J <63831720+PIYANAT-J@users.noreply.github.com>
2020-09-21 15:16:21 +0700

    21/09/63 15:17

---
+++ database/migrations/2020_09_21_095407_create_market_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMarketItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('market_items', function (Blueprint $table) {
+            $table->bigIncrements('item_id');
+            $table->string('item_name')->collation('utf8_unicode_ci');
+            $table->double('item_price', 15, 4)->default(0);
+            $table->double('item_discount', 10, 2)->default(0);
+            $table->string('item_img')->collation('utf8_unicode_ci');
+            $table->set('item_type', ['head', 'clothing', 'weapon'])->collation('utf8_unicode_ci');
+            $table->text('item_description')->collation('utf8_unicode_ci')->nullable();
+            $table->integer('item_level')->default(1);
+            $table->integer('item_amount')->default(1);
+            $table->set('item_status', ['true', 'false'])->collation('utf8_unicode_ci')->default('true');
+            $table->integer('USER_ID');
+            $table->string('USER_EMAIL')->collation('utf8_unicode_ci');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('market_items');
+    }
+}
